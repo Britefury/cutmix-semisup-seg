ccb4f407e82d2b9dfebe9140cfa6b1499c7fb26c
Ivan <ivan.grubisic.mail@gmail.com>
2022-01-26 11:02:09 +0100

    Add hubconf

---
+++ hubconf.py
@@ -0,0 +1,3 @@
+from .architectures.deeplab2 import (
+    ResNetDeepLab, resnet101_deeplab_coco, resnet101_deeplab_imagenet)
+from .architectures.deeplab3plus import DeepLabV3Plus, resnet101_deeplabv3plus_imagenet
